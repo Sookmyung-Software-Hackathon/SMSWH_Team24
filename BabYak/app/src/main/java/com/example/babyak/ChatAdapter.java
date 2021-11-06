0bb87a9fd169728ed63f774a773989968d618a98
mynzi1 <mynzi00@sookmyung.ac.kr>
2021-11-07 03:02:51 +0900

    Modify Chat

---
+++ BabYak/app/src/main/java/com/example/babyak/ChatAdapter.java
@@ -0,0 +1,107 @@
+package com.example.babyak;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+import java.util.ArrayList;
+
+public class ChatAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private ArrayList<ChatData> chatData;
+    private LayoutInflater inflater;
+    private String id;
+
+
+    public ChatAdapter(Context applicationContext, int chat_item, ArrayList<ChatData> list, String id) {
+        this.context = applicationContext;
+        this.layout = chat_item;
+        this.chatData = list;
+        // 레이아웃을 뷰로 바꾸기 위해(inflate) Context에서 inflater를 추출
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.id = id;
+    }
+
+    @Override
+    public int getCount() { // 전체 데이터 개수
+        return chatData.size();
+    }
+
+    @Override
+    public Object getItem(int position) { // position번째 아이템
+        return chatData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) { // position번째 항목의 id. 보통 position
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //항목의 index, 전에 inflate 되어있는 view, listView
+        //첫항목을 그릴때만 inflate 함 다음거부터는 매개변수로 넘겨줌 (느리기때문) : recycle
+        ViewHolder holder;
+
+        if(convertView == null){
+            //어떤 레이아웃을 만들어 줄 것인지, 속할 컨테이너, 자식뷰가 될 것인지
+            convertView = inflater.inflate(layout, parent, false); //아이디를 가지고 view를 만든다
+            holder = new ViewHolder();
+            holder.img = (ImageView)convertView.findViewById(R.id.pt_image);
+            holder.pt_msg = (TextView)convertView.findViewById(R.id.pt_content);
+            holder.pt_name = (TextView)convertView.findViewById(R.id.pt_id);
+            holder.card = (CardView)convertView.findViewById(R.id.pt_card);
+            holder.my_msg = (TextView)convertView.findViewById(R.id.my_content);
+
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        //누군지 판별
+        // 내 메시지일 때
+        if(chatData.get(position).getId().equals(id)){
+            // 상대방 메시지들을 안 보이게 함
+            holder.pt_name.setVisibility(View.GONE);
+            holder.pt_msg.setVisibility(View.GONE);
+            holder.img.setVisibility(View.GONE);
+            holder.card.setVisibility(View.GONE);
+
+            holder.my_msg.setVisibility(View.VISIBLE);
+
+            holder.my_msg.setText(chatData.get(position).getContent());
+        }else{
+            holder.pt_name.setVisibility(View.VISIBLE);
+            holder.pt_msg.setVisibility(View.VISIBLE);
+            holder.img.setVisibility(View.VISIBLE);
+            holder.card.setVisibility(View.VISIBLE);
+
+            // 내 메시지를 안 보이게 함
+            holder.my_msg.setVisibility(View.GONE);
+
+            holder.img.setImageResource(chatData.get(position).getImageID()); // 해당 사람의 프사 가져옴
+            holder.pt_msg.setText(chatData.get(position).getContent());
+            holder.pt_name.setText(chatData.get(position).getId());
+        }
+
+
+        return convertView;
+    }
+
+    //뷰홀더패턴
+    public class ViewHolder{
+        CardView card;
+        ImageView img;
+        TextView pt_msg;
+        TextView pt_name;
+        TextView my_msg;
+    }
+}
+
--- BabYak/app/src/main/java/com/example/babyak/ChatFragment.java
+++ BabYak/app/src/main/java/com/example/babyak/ChatFragment.java
@@ -1,26 +1,54 @@
 package com.example.babyak;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChatFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ChatFragment extends Fragment {
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-    // TODO: Rename parameter arguments, choose names that match
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ChatFragment extends Fragment {
+    ArrayList<ChatData> list = new ArrayList<>();
+    ListView lv;
+    Button sendBtn;
+    Button choiceBtn;
+    EditText edt;
+    int[] imageID = {R.drawable.ic_launcher_foreground};
+
+    String id = "";
+
+
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +56,6 @@ public class ChatFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ChatFragment newInstance(String param1, String param2) {
         ChatFragment fragment = new ChatFragment();
         Bundle args = new Bundle();
@@ -59,6 +78,93 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View v = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        lv = (ListView) v.findViewById(R.id.chat_listView);
+        edt = (EditText) v.findViewById(R.id.chat_editText);
+        sendBtn = (Button) v.findViewById(R.id.chat_sendBtn);
+        choiceBtn = (Button) v.findViewById(R.id.chat_choiceBtn);
+
+        // Firedb에 메시지 쓰기
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = db.getReference("message");
+
+        // 로그인한 아이디
+        id = "id";
+
+        final ChatAdapter ca = new ChatAdapter(getActivity().getApplicationContext(), R.layout.chat_item, list, id);
+        ((ListView) v.findViewById(R.id.chat_listView)).setAdapter(ca);
+
+
+        // 입력 버튼 클릭 시
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edt.getText().toString().equals("")) {
+                    Toast.makeText(getActivity().getApplicationContext(), "내용을 입력하세요.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    StringBuffer sb = new StringBuffer(edt.getText().toString());
+                    if (sb.length() > 15){
+                        for (int i = 0; i < sb.length() / 15; i++)
+                            sb.insert(15 * i, "\n");
+                    }
+
+                    myRef.push().setValue(new ChatData(R.drawable.ic_launcher_foreground, id, sb.toString()));
+                    edt.setText("");
+                }
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                ChatData value = snapshot.getValue(ChatData.class);
+                list.add(value);
+                ca.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // 좌측 선택 버튼 클릭 시 - 밥약
+        choiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getContext(), YesOrNo.class);
+                //startActivityForResult(intent, 1);
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == 1) {
+            Toast.makeText(getActivity(), "밥약 성공!!!", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(getActivity(), "밥약 실패...", Toast.LENGTH_LONG).show();
+        }
     }
 }
