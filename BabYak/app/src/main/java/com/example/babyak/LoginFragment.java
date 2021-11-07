97bcd388d79a31aaddaeeaf9f8ecc3108f65a481
mynzi1 <mynzi00@sookmyung.ac.kr>
2021-11-07 09:59:46 +0900

    Modify ChatFragment.java
    & Add LoginFragment

---
+++ BabYak/app/src/main/java/com/example/babyak/LoginFragment.java
@@ -0,0 +1,70 @@
+package com.example.babyak;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginFragment extends Fragment {
+    EditText user_id;
+    EditText user_pw;
+    Button btn_login;
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        user_id = (EditText) v.findViewById(R.id.user_id);
+        user_pw = (EditText) v.findViewById(R.id.user_pw);
+        btn_login = (Button) v.findViewById(R.id.login_btn);
+
+        GlobalVar gv = (GlobalVar) getActivity().getApplication();
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gv.setId(user_id.getText().toString());
+            }
+        });
+
+
+        return v;
+    }
+}
